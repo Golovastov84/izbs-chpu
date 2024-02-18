834dd897ca541506a298bbc7fd34ac186d270231
Golovastov84 <VSGolovastov@gmail.com>
2024-02-18 17:06:51 +0400

    frame_numbering_2.0

--- src/frame_numbering/Main.java
+++ src/frame_numbering/Main.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,11 +19,27 @@ public class Main {
 
         try {
             lines = Files.readAllLines(Paths.get(text));
+            SimpleDateFormat sdf = new SimpleDateFormat("y-M-d_H-m");
             Date now = new Date();
-            File file = new File("data/" + lines.get(0) + "_" + now.getTime() + ".nc");
+            File file = new File("data/" + lines.get(0).substring(2) + "_" + sdf.format(now) + ".nc");
             file.createNewFile();
             FileWriter writer = new FileWriter(file);
-            writer.write("text");
+            List<String> finalLines = new ArrayList<>();
+            String regex = "^N[0-9]+";
+            int i = 1;
+            for ( String line : lines
+            ) {
+                line=line.replaceAll(regex,"N" + i);
+                i++;
+                finalLines.add(line);
+                finalLines.add("\n");
+            }
+            String finallineStr = "";
+            for (String finalline : finalLines
+            ) {
+                finallineStr = finallineStr.concat(finalline);
+            }
+            writer.write(finallineStr);
             writer.flush();
             writer.close();
 
